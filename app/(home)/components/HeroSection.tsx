a918387fd7a2dca52d6dda391aa26bf95fcc8891
muskan <muskaann.129@gmail.com>
2024-04-07 14:49:41 +0530

    add: about remove: lorem 😂

--- app/(home)/components/HeroSection.tsx
+++ app/(home)/components/HeroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
     <div className='flex items-center justify-center min-h-[60vh]'>
       <div className='space-y-4 text-center'>
         <h1 className='font-bold text-7xl text-center md:text-8xl'><span className='text-rose-600'>Muskan</span> Gyanani</h1>
-        <p className='text-lg text-gray-400 text-center'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus obcaecati pariatur, magni inventore deleniti dolorem quos et sint alias maiores?</p>
+        <p className='text-lg text-gray-400 text-center'>A second year BTech student passionate about Computer Science and development. Proficient in front-end technologies and eager to delve into back-end development, I'm enthusiastic about learning new technologies to enhance my skills further.</p>
         <Link href={"mailto:muskaann.129@gmail.com"} className='inline-block'>
           <div>
             <Button
@@ -19,7 +19,7 @@ export default function HeroSection() {
           </div>
         </Link>
       </div>
-      <div className='h-8 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full'></div>
+      <div className='h-6 xl:h-32 bg-gradient-to-t from-black absolute -bottom-5 left-0 xl:bottom-0 w-full'></div>
     </div>
   )
 }
