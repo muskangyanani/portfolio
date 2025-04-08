62a006b0c9b4ff129157f5c0556aaa6b79617ea4
muskangyanani <muskaann.129@gmail.com>
2025-04-08 14:27:54 +0530

    Updated skills

--- app/(home)/components/Skills.tsx
+++ app/(home)/components/Skills.tsx
@@ -1,49 +1,51 @@
-"use client";
-import React from 'react'
-import { HoverEffect } from './ui/card-hover-effect'
-import { SiDjango, SiGit, SiGithub, SiJavascript, SiPython, SiReact, SiTailwindcss } from 'react-icons/si'
+'use client';
+import React from 'react';
+import { HoverEffect } from './ui/card-hover-effect';
+import {
+  SiTailwindcss,
+  SiJavascript,
+  SiReact,
+  SiPython,
+  SiDjango,
+  SiGit,
+  SiGithub,
+  SiHtml5,
+  SiNodedotjs,
+  SiExpress,
+  SiPostgresql,
+  SiTypescript,
+  // SiJava,
+  SiPostman,
+  SiChatbot,
+} from 'react-icons/si';
 
 export default function Skills() {
-
-  const skills =[
-    {
-      text: 'Tailwind CSS',
-      icon: SiTailwindcss,
-    },
-    {
-      text: 'JavaScript',
-      icon: SiJavascript,
-    },
-    {
-      text: 'React',
-      icon: SiReact,
-    },
-    {
-      text:'Python',
-      icon: SiPython,
-    },
-    {
-      text:'Django',
-      icon: SiDjango,
-    },
-    {
-      text: 'Django Rest Framework',
-      icon: SiDjango,
-    },
-    {
-      text: 'Git',
-      icon: SiGit,
-    },
-    {
-      text: 'GitHub',
-      icon: SiGithub,
-    },
-  ]
+  const skills = [
+    { text: 'HTML', icon: SiHtml5 },
+    { text: 'Tailwind CSS', icon: SiTailwindcss },
+    { text: 'React.js', icon: SiReact },
+    { text: 'JavaScript', icon: SiJavascript },
+    { text: 'TypeScript (Learning)', icon: SiTypescript },
+    { text: 'Python', icon: SiPython },
+    // { text: 'Java', icon: SiJava },
+    { text: 'Node.js', icon: SiNodedotjs },
+    { text: 'Express.js', icon: SiExpress },
+    { text: 'Django', icon: SiDjango },
+    { text: 'Django Rest Framework', icon: SiDjango },
+    { text: 'SQL', icon: SiPostgresql },
+    { text: 'REST APIs', icon: SiPostman },
+    { text: 'Git', icon: SiGit },
+    { text: 'GitHub', icon: SiGithub },
+    { text: 'ChatGPT', icon: SiChatbot }, // Placeholder icon
+    { text: 'Postman', icon: SiPostman },
+  ];
 
   return (
-    <div className='max-w-5xl mx-auto px-8 mt-10'>
-      <h1 className='sm:text-5xl text-3xl font-bold underline underline-offset-4 decoration-rose-600 text-center'><span className='text-rose-600'>S</span>kills</h1>
-      <HoverEffect items={skills}/>
+    <div className="max-w-5xl mx-auto px-8 mt-10">
+      <h1 className="sm:text-5xl text-3xl font-bold underline underline-offset-4 decoration-rose-600 text-center">
+        <span className="text-rose-600">S</span>kills
+      </h1>
+      <HoverEffect items={skills} />
     </div>
-  )
+  );
 }
--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
-import Navbar from './components/Navbar'
-import HeroSection from './components/HeroSection'
-import { Spotlight } from "./components/ui/Spotlight";
+import React from 'react';
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
+import { Spotlight } from './components/ui/Spotlight';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 
 export default function page() {
   return (
-    <div className='min-h-screen bg-black'>
+    <div className="min-h-screen bg-black">
       <Spotlight
         className="-top-40 left-0 md:left-60 md:-top-20"
         fill="white"
       />
-      <div className='dark:bg-black h-screen bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative'>
+      <div className="dark:bg-black h-screen bg-white  dark:bg-grid-white/[0.05] bg-grid-black/[0.2] relative">
         <div className="max-w-6xl mx-auto p-5 ">
-          <Navbar/>
-          <HeroSection/>
+          <Navbar />
+          <HeroSection />
         </div>
       </div>
-      <Skills/>
-      <Projects/>
-      <Footer/>
+      <Skills />
+      <Projects />
+      <Footer />
     </div>
-  )
+  );
 }
